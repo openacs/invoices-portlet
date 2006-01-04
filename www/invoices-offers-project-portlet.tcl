52e003952ad0f6db49e7658a05127fb7c40c8fba
Timo Hentschel <timo@timohentschel.de>
2006-01-04 08:42:20 +0000

    fixed offer portlet

--- www/invoices-offers-project-portlet.tcl
+++ www/invoices-offers-project-portlet.tcl
@@ -1,4 +1,4 @@
-set row_list {offer_nr {} title {} description {} amount_total {} creation_user {} creation_date {} finish_date {} action {}}
+set row_list {title {} project_contact {} finish_date {} offer_nr {} amount_total {} creation_user {} creation_date {} action {}}
 
 set user_id [ad_conn user_id]
 set community_id [dotlrn_community::get_community_id_from_url]
